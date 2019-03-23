ab3d9ad4745f8d9419e50f84e7699c45f6839fda
Jose Carlos Nuñez Valverde <josek21nunez@gmail.com>
2019-03-23 17:59:17 -0500

    feature: Components with html code inside

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelpComponent } from './help/help.component';
+import { HistoryComponent } from './history/history.component';
+import { MatematicalStepsComponent } from './matematical-steps/matematical-steps.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  MainComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'matematicalSteps', component: MatematicalStepsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HistoryComponent } from './history/history.component';
 import { HelpComponent } from './help/help.component';
 import { MatematicalStepsComponent } from './matematical-steps/matematical-steps.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HistoryComponent,
     HelpComponent,
-    MatematicalStepsComponent
+    MatematicalStepsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
