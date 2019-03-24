514890832d5e82d5dc16d17ea0ce5955b3b680b2
Jose Carlos Nuñez Valverde <josek21nunez@gmail.com>
2019-03-24 00:55:14 -0500

    Feature: Add logical functions

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  validate:number = 0;
+  mag:number = 0;
+  ang:number=0;
+
+  solutions:Array<String> = [];
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {    
   }
 
+  /**
+   * checkNumber
+   */
+  public checkNumber(a:number,b:number,r:number) {    
+    if ( (a && b && r) > 0 ){
+      this.validate=1;
+      this.magnitude(a,b);
+      this.angle(a,b);
+      this.createSolution(r);
+    }else{
+      this.validate=0;
+    }
+    console.log(this.validate);
+  }
+
+  /**
+   * magnitude
+   */
+  public magnitude(a:number,b:number) {
+    
+    this.mag= Math.pow(a*a + b*b,0.5);
+    console.log(this.mag);
+  }
+
+  /**
+   * angle
+   */
+  public angle(a:number,b:number) {
+   this.ang= Math.atan(b/a)*(180/Math.PI);
+   console.log(this.ang);
+  }
+
+  /**
+   * createSolution
+   */
+  public createSolution(n:number) {
+
+    for (let index = 0; index < n; index++) {
+     this.solutions.push(index.toString());
+    }
+    console.log(this.solutions);
+    this.solutions = [];
+  }
 }
